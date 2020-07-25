390de4fe4ffc84276a11a38fb2a60454466369af
tckz916 <tachikaze0916@gmail.com>
2020-07-26 03:32:37 +0900

    Update Paper #92 -> #114 FastAsyncWorldtEdit #302 -> #306

--- setup.bat
+++ setup.bat
@@ -5,8 +5,8 @@ if exist %CD%\plugins\fastasyncvoxelsniper.jar del %CD%\plugins\fastasyncvoxelsn
 if exist %CD%\plugins\multiverse-core.jar del %CD%\plugins\multiverse-core.jar
 if exist %CD%\plugins\cleanroomgenerator.jar del %CD%\plugins\cleanroomgenerator.jar
 if exist %CD%\paper.jar del %CD%\paper.jar
-curl -o %CD%\paper.jar https://papermc.io/api/v1/paper/1.16.1/92/download
-curl -o  %CD%\plugins\fastasynworldedit.jar https://ci.athion.net/job/FastAsyncWorldEdit-1.16/lastSuccessfulBuild/artifact/worldedit-bukkit/build/libs/FastAsyncWorldEdit-1.16-302.jar
+curl -o %CD%\paper.jar https://papermc.io/api/v1/paper/1.16.1/114/download
+curl -o  %CD%\plugins\fastasynworldedit.jar https://ci.athion.net/job/FastAsyncWorldEdit-1.16/lastSuccessfulBuild/artifact/worldedit-bukkit/build/libs/FastAsyncWorldEdit-1.16-306.jar
 curl -o %CD%\plugins\fastasyncvoxelsniper.jar https://ci.athion.net/job/FastAsyncVoxelSniper-flattening/lastSuccessfulBuild/artifact/build/libs/FastAsyncVoxelSniper-flattened-1.0.3-backward.jar 
 curl -o %CD%\plugins\multiverse-core.jar https://media.forgecdn.net/files/2768/599/Multiverse-Core-4.1.0.jar 
 curl -o %CD%\plugins\cleanroomgenerator.jar https://media.forgecdn.net/files/2615/372/CleanroomGenerator-1.1.0.jar
